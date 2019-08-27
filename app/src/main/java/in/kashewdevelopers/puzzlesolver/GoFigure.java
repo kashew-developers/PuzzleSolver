9cc2475c606e626fa1478c7af8b441a6da2bbf2d
kashew-developers <kashewdevelopers@gmail.com>
2019-08-27 21:08:04 +0530

    Make GoFigure Functional

--- app/src/main/java/in/kashewdevelopers/puzzlesolver/GoFigure.java
+++ app/src/main/java/in/kashewdevelopers/puzzlesolver/GoFigure.java
@@ -1,15 +1,151 @@
 package in.kashewdevelopers.puzzlesolver;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Stack;
+import java.util.Vector;
 
 public class GoFigure extends AppCompatActivity {
+
+    EditText input_1, input_2, input_3, input_4, answer;
+    TextView operator_1, operator_2, operator_3;
+    Button solve_button;
+    Toast invalid_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_figure);
+
+        // Initialize Valiables
+        input_1 = findViewById(R.id.box1);
+        input_2 = findViewById(R.id.box2);
+        input_3 = findViewById(R.id.box3);
+        input_4 = findViewById(R.id.box4);
+
+        answer = findViewById(R.id.box5);
+
+        operator_1 = findViewById(R.id.operator1);
+        operator_2 = findViewById(R.id.operator2);
+        operator_3 = findViewById(R.id.operator3);
+
+        solve_button = findViewById(R.id.solveButton);
+
+        invalid_input = Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT);
+
+    }
+
+    public int getOperatorPriority(String operator){
+
+        switch( operator ){
+            case "+":
+            case "-": return 0;
+            case "*":
+            case "/": return 1;
+        }
+
+        return 0;
+    }
+
+    public boolean evaluateEquation(String[] equation, int target){
+
+        Stack<String> result = new Stack<>();
+        Stack<String> operators = new Stack<>();
+
+        for(int i = 0; i < equation.length; i++){
+
+            if( i % 2 == 0 ){
+                result.push(equation[i]);
+            }
+            else {
+
+                while ( ! operators.empty() && getOperatorPriority(operators.peek()) >= getOperatorPriority(equation[i])) {
+
+                    int value2 = Integer.parseInt(result.pop());
+                    int value1 = Integer.parseInt(result.pop());
+                    String previous_operator = operators.pop();
+
+                    if (previous_operator.equals("+")) value1 += value2;
+                    else if (previous_operator.equals("-")) value1 -= value2;
+                    else if (previous_operator.equals("*")) value1 *= value2;
+                    else if (previous_operator.equals("/")) try {value1 /= value2;} catch (Exception e){ value1 = 0;}
+
+                    result.push(String.valueOf(value1));
+                }
+                operators.push(equation[i]);
+
+            }
+
+        }
+
+        while( ! operators.empty() ){
+
+            int value2 = Integer.parseInt(result.pop());
+            int value1 = Integer.parseInt(result.pop());
+            String previous_operator = operators.pop();
+
+            if( previous_operator.equals("+") ) value1 += value2;
+            else if( previous_operator.equals("-") ) value1 -= value2;
+            else if( previous_operator.equals("*") ) value1 *= value2;
+            else if( previous_operator.equals("/") ) try {value1 /= value2;} catch (Exception e){ value1 = 0;}
+
+            result.push( String.valueOf(value1) );
+        }
+
+        return target == Integer.parseInt( result.peek() );
+    }
+
+    public void incrementOperators(int[] oprators){
+
+        int i = 2;
+        oprators[i]++;
+        while ( i > 0 && oprators[i] > 3 ){
+            oprators[i] = 0;
+            oprators[--i]++;
+        }
+
+    }
+
+    public void onSolveClick(View v){
+
+        // Check if all required inputs (5 values) are given
+        // if not, show toast & exit function
+        if( input_1.getText().toString().equals("") || input_2.getText().toString().equals("") ||
+                input_3.getText().toString().equals("") || input_4.getText().toString().equals("") ||
+                answer.getText().toString().equals("") ){
+            invalid_input.show();
+            return;
+        }
+
+        String[] equation = {input_1.getText().toString(), "+", input_2.getText().toString(), "+",
+                                input_3.getText().toString(), "+", input_4.getText().toString()};
+        String[] operatorMapping = {"+", "-", "*", "/"};
+        int[] operators = {0, 0, 0};
+        int target = Integer.parseInt( answer.getText().toString() );
+
+        while( operators[0] <= 3 ){
+
+            equation[1] = operatorMapping[ operators[0] ];
+            equation[3] = operatorMapping[ operators[1] ];
+            equation[5] = operatorMapping[ operators[2] ];
+
+            if( evaluateEquation(equation, target) ) {
+                operator_1.setText( equation[1] );
+                operator_2.setText( equation[3] );
+                operator_3.setText( equation[5] );
+                break;
+            }
+
+            incrementOperators(operators);
+
+        }
+
     }
 }
