6efed588a59cb7b984e26a16577307966954e4e2
kashew-developers <kashewdevelopers@gmail.com>
2019-09-01 07:48:38 +0530

    Add Puzzle selection option on MainActivity

--- app/src/main/java/in/kashewdevelopers/puzzlesolver/MainActivity.java
+++ app/src/main/java/in/kashewdevelopers/puzzlesolver/MainActivity.java
@@ -1,17 +1,30 @@
 package in.kashewdevelopers.puzzlesolver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView goFigureCard, sudokuCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, GoFigure.class));
+        goFigureCard = findViewById(R.id.goFigureCard);
+        sudokuCard = findViewById(R.id.sudokuCard);
+
+        goFigureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GoFigure.class));
+            }
+        });
+
     }
 }
